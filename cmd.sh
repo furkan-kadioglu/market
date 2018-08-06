81b08c52744f781281f4378c430ff9cbcf373cf4
Furkan Kadıoğlu <32355872+furkan-kadioglu@users.noreply.github.com>
2018-08-06 20:57:20 +0300

    update cmd

--- cmd.sh
+++ cmd.sh
@@ -1,12 +1,12 @@
-docker run  --name pg -d -p 5432:5432 -e POSTGRES_PASSWORD=admin postgres:10.4 
+#docker run  --name pg -d -p 5432:5432 -e POSTGRES_PASSWORD=admin postgres:10.4 
 
-apt update  
+#apt update  
 
-apt install -y python3-pip
+#apt install -y python3-pip
 
-apt install -y python3-tk
+#apt install -y python3-tk
 
-pip3 install pipenv==11.10.0
+#pip3 install pipenv==11.10.0
 
 pipenv install --deploy
 
